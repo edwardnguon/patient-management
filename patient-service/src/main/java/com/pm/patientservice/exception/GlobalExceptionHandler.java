2a3121e5462d69073754229da8d4010912c60649
edwardnguon <edwardnguon@gmail.com>
2025-05-07 21:33:11 -0400

    Boiler plate patient api stuff, get/post patients, exception handling

---
+++ patient-service/src/main/java/com/pm/patientservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.pm.patientservice.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+  private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationException(
+          MethodArgumentNotValidException exception) {
+    Map<String, String> fieldErrors = new HashMap<>();
+
+    exception.getBindingResult().getFieldErrors().forEach(
+            fieldError -> fieldErrors.put(fieldError.getField(), fieldError.getDefaultMessage()));
+
+    return ResponseEntity.badRequest().body(fieldErrors);
+  }
+
+  @ExceptionHandler(EmailAlreadyExistsException.class)
+  public ResponseEntity<Map<String, String>> handleEmailAlreadyExistsException(
+          EmailAlreadyExistsException exception
+  ) {
+    log.warn("Email address already exists {}", exception.getMessage());
+
+    Map<String, String> errors = new HashMap<>();
+    errors.put("message", "Email already exists");
+    return ResponseEntity.badRequest().body(errors);
+  }
+}
